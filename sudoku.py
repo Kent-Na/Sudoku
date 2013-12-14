67bcb609ec878973d780d9628f54bbe5f5d78e2d
YukiYasuda <yuki_yasuda_survey@docomo.ne.jp>
2013-12-15 05:52:07 +0900

    add simple_solver with some bugs

--- sudoku.py
+++ sudoku.py
@@ -18,8 +18,8 @@ sudoku_ans_filename = 'sudoku_anses.pkl'
 
 ### basic functions
 
-def clone_sudoku(sudoku):
-    return [[sudoku[j][i] for i in range(ROW)] for j in range(ROW)]
+def clone_sudoku(sudoku_mat):
+    return [[sudoku_mat[j][i] for i in range(ROW)] for j in range(ROW)]
 
 def defined_matrix(sudoku_mat):
     sudoku_mat = clone_sudoku(sudoku_mat)
@@ -50,32 +50,32 @@ def is_legal_matrix(sudoku_mat):
                 return False
     return True
 
-def get_candidates(sudoku,i,j):
+def get_candidates(sudoku_mat,i,j):
     """
-    return cadidate at sudoku.(i,j)
-    if sudoku.(i,j) is defined, return None.
+    return cadidate at sudoku_mat.(i,j)
+    if sudoku_mat.(i,j) is defined, return None.
     """
-    sudoku = clone_sudoku(sudoku)
-    if sudoku[i][j] != unknown:
+    sudoku_mat = clone_sudoku(sudoku_mat)
+    if sudoku_mat[i][j] != unknown:
         return None
 
     candidates = set(range(1,10))
-    vertical = set(sudoku[i])
-    yoko     = set([sudoku[k][j] for k in range(ROW)])
-    cell     = set([sudoku[i//3*3 + k][j//3*3 + l]\
+    vertical = set(sudoku_mat[i])
+    yoko     = set([sudoku_mat[k][j] for k in range(ROW)])
+    cell     = set([sudoku_mat[i//3*3 + k][j//3*3 + l]\
                 for k in range(SIZE) for l in range(SIZE)])
     candidates = candidates.difference(vertical)
     candidates = candidates.difference(yoko)
     candidates = candidates.difference(cell)
     return candidates
 
-def get_all_candidates(sudoku):
+def get_all_candidates(sudoku_mat):
     """
     """
     d_candidates = {}
     for i in range(ROW):
         for j in range(ROW):
-            candidates = get_candidates(sudoku, i, j)
+            candidates = get_candidates(sudoku_mat, i, j)
             if candidates:
                 d_candidates[(i,j)] = candidates
 
@@ -144,10 +144,63 @@ def solverule3(sudoku_mat,i,j):
 
 solve_rules = (solverule1, solverule2, solverule3)
 
+def solve_simple_sudoku(sudoku_mat):
+    print("I'm here!!")
+    print(sudoku_repr(sudoku_mat))
+    sudoku_mat = clone_sudoku(sudoku_mat)
+    d_candiates = get_all_candidates(sudoku_mat)
+
+    # if number of candiate == 1, decide at this point.
+    for undef_ind in d_candiates:
+        if len(d_candiates[undef_ind]) == 1:
+            i, j = undef_ind
+            candiate = d_candiates[undef_ind].pop()
+            sudoku_mat[i][j] = candiate
+            print('Tester')
+            return solve_simple_sudoku(sudoku_mat)
+
+    for undef_ind in d_candiates:
+        i, j = undef_ind
+        undef_set = d_candiates[undef_ind]
+        
+        vertical_defined_set = set()
+        for l in range(ROW):
+            if  l != j and (i,l) in d_candiates:
+                vertical_defined_set = vertical_defined_set.union(d_candiates[(i,l)])
+        for candiate in d_candiates[undef_ind]:
+            if not candiate in vertical_defined_set:
+                sudoku_mat[i][j] = candiate
+                return solve_simple_sudoku(sudoku_mat)
+        undef_set = d_candiates[undef_ind]
+        
+        horizontan_defined_set = set()
+        for k in range(ROW):
+            if k != i and (k,j) in d_candiates:
+                horizontan_defined_set = horizontan_defined_set.union(d_candiates[(k,j)])
+        for candiate in d_candiates[undef_ind]:
+            if not candiate in horizontan_defined_set:
+                sudoku_mat[i][j] = candiate
+                return solve_simple_sudoku(sudoku_mat)
+        undef_set = d_candiates[undef_ind]
+
+        cell_defined_set = set()
+        for k in range(SIZE):
+            for l in range(SIZE):
+                if (i//3*3 + k != i and j//3*3 + l != j) and \
+                        (i//3*3 + k, j//3*3+l) in d_candiates[undef_ind]:
+                    cell_defined_set = cell_defined_set.union(
+                            d_candiates[(i//3*3+k)][j//3*3+k])
+        for candiate in d_candiates[undef_ind]:
+            if not candiate in cell_defined_set:
+                sudoku_mat[i][j] = candiate
+                return solve_simple_sudoku(sudoku_mat)
+    print(sudoku_repr(sudoku_mat))
+    return sudoku_mat
+                
 def solve_basic_sudoku(sudoku_mat):
     """
-    Solve sudoku.
-    return a solution of sudoku.
+    Solve sudoku_mat.
+    return a solution of sudoku_mat.
     """
     sudoku_mat = clone_sudoku(sudoku_mat)
     is_finished = False
@@ -165,28 +218,28 @@ def solve_basic_sudoku(sudoku_mat):
                         #print('define {},{}'.format(i,j))
         if old_mat == sudoku_mat:
             is_finished = True
-    return sudoku_mat
+    return solve_simple_sudoku(sudoku_mat)
 
 ## solve by recursive method
 
-def solve_sudoku_by_recursive(sudoku, cnt=0):  # todo: with mistake?
-    #print(sudoku)
-    sudoku = clone_sudoku(sudoku)
-    #print(sudoku)
+def solve_sudoku_by_recursive(sudoku_mat, cnt=0):  # todo: with mistake?
+    print('Recursive')
+    sudoku_mat = clone_sudoku(sudoku_mat)
+    #print(sudoku_mat)
 
-    d_candiate = get_all_candidates(sudoku)
+    d_candiate = get_all_candidates(sudoku_mat)
     if len(d_candiate) == 0:
-        if is_legal_matrix(sudoku):
-            return sudoku
+        if is_legal_matrix(sudoku_mat):
+            return sudoku_mat
         else:
-            #print(sudoku_repr(sudoku))
+            #print(sudoku_repr(sudoku_mat))
             return None
 
     undef_ind, candiates = list(d_candiate.items())[0]
-    if cnt < 10:
+    if cnt < 20:
         print('{}th loop is started.'.format(cnt))
     for candiate in candiates:
-        sol = [[sudoku[j][i] for i in range(ROW)] for j in range(ROW)]
+        sol = [[sudoku_mat[j][i] for i in range(ROW)] for j in range(ROW)]
         sol[undef_ind[0]][undef_ind[1]] = candiate
 
         sol = solve_basic_sudoku(sol)
@@ -199,30 +252,30 @@ def solve_sudoku_by_recursive(sudoku, cnt=0):  # todo: with mistake?
             return sol
         elif defined_matrix(sol):
             print("This question has no solution.")
-    if cnt < 10:
+    if cnt < 20:
         print('{}th loop is finished.'.format(cnt))
     return None
         
-def solve_sudoku(sudoku):
-    sudoku = clone_sudoku[sudoku_mat]
-    sudoku = solve_basic_sudoku(sudoku)
-    return solve_sudoku_by_recursive(sudoku)
+def solve_sudoku(sudoku_mat):
+    sudoku_mat = clone_sudoku(sudoku_mat)
+    sudoku_mat = solve_basic_sudoku(sudoku_mat)
+    return solve_sudoku_by_recursive(sudoku_mat)
 
 ### functions for main
                 
 def load_sudoku(filename):
-    sudoku = [[0 for i in range(ROW)] for j in range(ROW)]
+    sudoku_mat = [[0 for i in range(ROW)] for j in range(ROW)]
     with open(filename) as f:
         a_sudoku = list(trim(f.read()))
         for i,c in enumerate(a_sudoku):
             if c.isdigit() is True:
                 c = int(c)
-            sudoku[i // ROW][i % ROW] = c
-    return sudoku
+            sudoku_mat[i // ROW][i % ROW] = c
+    return sudoku_mat
 
-def sudoku_repr(sudoku):
+def sudoku_repr(sudoku_mat):
     s = ''
-    for row in sudoku:
+    for row in sudoku_mat:
         for cell in row:
             s += str(cell)
         s += '\n'
@@ -230,16 +283,16 @@ def sudoku_repr(sudoku):
 
 def is_legal_test():
     filename = 'ans.dat'
-    sudoku = load_sudoku(filename)
-    print(is_legal_matrix(sudoku))
+    sudoku_mat = load_sudoku(filename)
+    print(is_legal_matrix(sudoku_mat))
 
 ### main
 def main(filename):
-    sudoku = load_sudoku(filename)
+    sudoku_mat = load_sudoku(filename)
     print('give: ' + '=' * 9)
-    print(sudoku_repr(sudoku))
+    print(sudoku_repr(sudoku_mat))
     
-    sol = solve_sudoku_by_recursive(sudoku)
+    sol = solve_sudoku_by_recursive(sudoku_mat)
     if not is_legal_matrix(sol):
         print('The question can\'t solved.')
     else:
@@ -248,6 +301,10 @@ def main(filename):
 
 def sudoku_test():
     main('difficult.dat')
+
+def solve_simple_sudoku_test():
+    sudoku_mat = load_sudoku('easy_question.dat')
+    print(sudoku_repr(solve_simple_sudoku(sudoku_mat)))
 
 if __name__ == '__main__':
     is_test = False
